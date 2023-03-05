abfd19c3371f34a5de27860145aa230283198c62
Karl Heitmann <khf1988@gmail.com>
2023-03-05 01:35:54 -0300

    shows head and branches

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use git2::Repository;
+use git2::BranchType;
+
 fn main() {
     println!("Hello, world!");
+    let repo = match Repository::open(".") {
+        Ok(repo) => repo,
+        Err(e) => panic!("failed to open: {}", e),
+    };
+    let head = match repo.head() {
+        Ok(repo) => repo,
+        Err(e) => panic!("failed to get head: {}", e),
+    };
+    // debug!(logger, "head found"; "head" => head.name());
+
+    println!("{:?}", head.name());
+    println!("{:?}", head.shorthand());
+    println!("{:?}", head.is_branch());
+
+    for branch in repo.branches(Some(BranchType::Local)).unwrap() {
+        let b = branch.unwrap();
+        println!("{:?}", b.0.get().name());
+        println!("{:?}", b.0.get().shorthand());
+    }
+    // println!("{:?}", repo.branches());
 }
