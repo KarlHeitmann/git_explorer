748b84b5d09af5cd07c9c746425030ba2de3b08a
Karl Heitmann <khf1988@gmail.com>
2023-03-06 00:21:11 -0300

    Graph shows ({id})

--- src/graph.rs
+++ src/graph.rs
@@ -1,9 +1,15 @@
 use git2::{Commit, Oid};
 
+fn find_common_node() { todo!(); }
+
 fn paint_branch_aux(commit: Commit, oid: Oid, offset: u8) {
     if commit.id() != oid {
         println!("│ │");
-        println!("│ ● {}", commit.summary().unwrap());
+        // commit.id().to
+        unsafe {
+            let id = commit.id().to_string();
+            println!("│ ● ({}) {} ", id.get_unchecked(0..6), commit.summary().unwrap());
+        }
         match commit.parent(0) {
             Ok(parent) => paint_branch_aux(parent, oid, offset),
             Err(e) => println!("ERROR: commit {} has error on getting parent: {}", commit.id(), e),
@@ -19,12 +25,17 @@ fn paint_branch(commit: Commit, oid: Option<Oid>, offset: u8) {
             paint_branch_aux(commit, oid, 0);
         },
         None => {
-            println!("│");
-            println!("● {}", commit.summary().unwrap());
+            unsafe {
+                let id = commit.id().to_string();
+                println!("● ({}) {} ", id.get_unchecked(0..6), commit.summary().unwrap());
+            }
             let parents: Vec<Commit> = commit.parents().collect();
             match parents.len() {
-                0 => {},
-                1 => paint_branch(parents[0].clone(), None, 0),
+                0 => { println!("---No parent---") },
+                1 => {
+                    println!("│");
+                    paint_branch(parents[0].clone(), None, 0)
+                },
                 2 => {
                     let parent_1 = &parents[0];
                     let parent_2 = &parents[1];
@@ -39,27 +50,6 @@ fn paint_branch(commit: Commit, oid: Option<Oid>, offset: u8) {
 }
 
 pub fn paint_commit_track(commit: Commit) {
-    match commit.summary() {
-        Some(message) => println!("● {}", message.trim()),
-        None => {},
-    }
-
-    let parents: Vec<Commit> = commit.parents().collect();
-    match parents.len() {
-        0 => {},
-        1 => {
-            paint_branch(parents[0].clone(), None, 0);
-        },
-        2 => {
-            // NOTE left parent (parents[0]) is the far away commit, right parent (parents[1]) is the closes commit
-            println!("2 PARENTS!");
-            let parent_1 = &parents[0];
-            let parent_2 = &parents[1];
-            paint_branch(parent_1.clone(), None, 0);
-            paint_branch(parent_2.clone(), Some(parent_1.id()), 0);
-
-        }
-        _ => {},
-    }
+    paint_branch(commit, None, 0);
 }
 
