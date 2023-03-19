d62e36e8b0259d932846dc98d2ae9926b6761aed
Karl Heitmann <khf1988@gmail.com>
2023-03-19 02:21:07 -0300

    Created app.rs file

---
+++ src/ui/app.rs
@@ -0,0 +1,113 @@
+use git2::Repository;
+
+use tui::{
+    layout::{Alignment, Constraint, Direction, Layout, Rect},
+    style::{Color, Modifier, Style},
+    terminal::Frame,
+    text::{Span, Spans, Text},
+    backend::Backend,
+    widgets::{
+        Block, BorderType, Borders, ListState, Paragraph, Tabs
+    },
+};
+
+use crate::graph::GitExplorer;
+use crate::ui::home::wrapper;
+
+#[derive(Copy, Clone, Debug)]
+pub enum MenuItem {
+    Home,
+    Nodes,
+    Edit,
+    SubSearch
+}
+
+impl From<MenuItem> for usize {
+    fn from(input: MenuItem) -> usize {
+        match input {
+            MenuItem::Home => 0,
+            MenuItem::Nodes => 1,
+            MenuItem::Edit => 2,
+            MenuItem::SubSearch => 3,
+        }
+    }
+}
+
+fn get_layout_chunks(size: Rect) -> Vec<Rect> {
+    Layout::default()
+        .direction(Direction::Vertical)
+        .margin(2)
+        .constraints(
+            [
+                Constraint::Length(3),
+                Constraint::Min(2),
+                Constraint::Length(3),
+            ]
+            .as_ref(),
+        )
+        .split(size)
+}
+
+fn draw_status_bar<'layout>() -> Paragraph<'layout> {
+    let (title, color) = ("NORMAL MODE +++FILTER MODE CONTAIN+++", Color::LightCyan);
+
+    Paragraph::new(title)
+        .style(Style::default().fg(color))
+        .alignment(Alignment::Center)
+        .block(
+            Block::default()
+                .borders(Borders::ALL)
+                .style(Style::default().fg(Color::White))
+                .title("Status")
+                .border_type(BorderType::Plain),
+        )
+}
+
+fn draw_menu_tabs<'a>(menu_titles: &'a Vec<&'a str>, active_menu_item: MenuItem) -> Tabs<'a> {
+    let menu = menu_titles
+        .iter()
+        .map(|t| {
+            let (first, rest) = t.split_at(1);
+            Spans::from(vec![
+                Span::styled(
+                    first,
+                    Style::default()
+                        .fg(Color::Yellow)
+                        .add_modifier(Modifier::UNDERLINED),
+                ),
+                Span::styled(rest, Style::default().fg(Color::White)),
+            ])
+        })
+        .collect();
+
+    Tabs::new(menu)
+        .select(active_menu_item.into())
+        .block(Block::default().title("Menu").borders(Borders::ALL))
+        .style(Style::default().fg(Color::White))
+        .highlight_style(Style::default().fg(Color::Yellow))
+        .divider(Span::raw("|"))
+}
+
+pub fn app<B: Backend>(
+    f: &mut Frame<B>,
+    node_list_state: &mut ListState,
+    git_explorer: &GitExplorer,
+    repo: &Repository,
+    percentage_left: u16, percentage_right: u16) {
+    let menu_titles = vec!["Home", "Quit"];
+    let active_menu_item = MenuItem::Home;
+
+    let mut chunks = get_layout_chunks(f.size());
+
+    let status_bar = draw_status_bar();
+
+    let tabs = draw_menu_tabs(&menu_titles, active_menu_item);
+
+    f.render_widget(tabs, chunks[0]);
+
+    wrapper(f, percentage_left, percentage_right, node_list_state, &mut chunks, &git_explorer, repo);
+
+    f.render_widget(status_bar, chunks[2]);
+
+}
+
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -3,19 +3,14 @@ use git2::{Repository, Oid};
 
 use crate::graph::GraphNode;
 use crate::{utils::short_id, graph::GitExplorer};
-use crate::ui::home::wrapper;
 
 mod home;
+mod app;
 
 use tui::{
-    layout::{Alignment, Constraint, Direction, Layout, Rect},
-    style::{Color, Modifier, Style},
-    text::{Span, Spans, Text},
+    text::{Spans, Text},
     backend::Backend,
-    widgets::{
-        Block, BorderType, Borders, ListState, Paragraph, Tabs
-    },
-
+    widgets::ListState,
     Terminal
 };
 
@@ -55,87 +50,13 @@ impl From<&GraphNode> for Text<'_> {
     }
 }
  
-#[derive(Copy, Clone, Debug)]
-pub enum MenuItem {
-    Home,
-    Nodes,
-    Edit,
-    SubSearch
-}
 
-impl From<MenuItem> for usize {
-    fn from(input: MenuItem) -> usize {
-        match input {
-            MenuItem::Home => 0,
-            MenuItem::Nodes => 1,
-            MenuItem::Edit => 2,
-            MenuItem::SubSearch => 3,
-        }
-    }
-}
-
-pub fn get_layout_chunks(size: Rect) -> Vec<Rect> {
-    Layout::default()
-        .direction(Direction::Vertical)
-        .margin(2)
-        .constraints(
-            [
-                Constraint::Length(3),
-                Constraint::Min(2),
-                Constraint::Length(3),
-            ]
-            .as_ref(),
-        )
-        .split(size)
-}
-
-pub fn draw_status_bar<'layout>() -> Paragraph<'layout> {
-    let (title, color) = ("NORMAL MODE +++FILTER MODE CONTAIN+++", Color::LightCyan);
-
-    Paragraph::new(title)
-        .style(Style::default().fg(color))
-        .alignment(Alignment::Center)
-        .block(
-            Block::default()
-                .borders(Borders::ALL)
-                .style(Style::default().fg(Color::White))
-                .title("Status")
-                .border_type(BorderType::Plain),
-        )
-}
-
-pub fn draw_menu_tabs<'a>(menu_titles: &'a Vec<&'a str>, active_menu_item: MenuItem) -> Tabs<'a> {
-    let menu = menu_titles
-        .iter()
-        .map(|t| {
-            let (first, rest) = t.split_at(1);
-            Spans::from(vec![
-                Span::styled(
-                    first,
-                    Style::default()
-                        .fg(Color::Yellow)
-                        .add_modifier(Modifier::UNDERLINED),
-                ),
-                Span::styled(rest, Style::default().fg(Color::White)),
-            ])
-        })
-        .collect();
-
-    Tabs::new(menu)
-        .select(active_menu_item.into())
-        .block(Block::default().title("Menu").borders(Borders::ALL))
-        .style(Style::default().fg(Color::White))
-        .highlight_style(Style::default().fg(Color::Yellow))
-        .divider(Span::raw("|"))
-}
 
 
 // fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
 
 // pub fn explorer_wrapper<B: Backend>(terminal: &mut Terminal<B>, repo: &Repository, root_commit: Commit, stop_condition: Option<(Oid, String)>) -> Result<(), Box<dyn std::error::Error>> {
 pub fn explorer_wrapper<B: Backend>(terminal: &mut Terminal<B>, repo: &Repository, stop_condition: Option<(Oid, String)>) -> Result<(), Box<dyn std::error::Error>> {
-    let menu_titles = vec!["Home", "Quit"];
-    let active_menu_item = MenuItem::Home;
     let mut node_list_state = ListState::default();
     let mut git_explorer = GitExplorer::new(None, None, stop_condition.clone()); // TARGET
     git_explorer.run();
@@ -147,17 +68,7 @@ pub fn explorer_wrapper<B: Backend>(terminal: &mut Terminal<B>, repo: &Repositor
     terminal.clear()?;
     loop {
         terminal.draw(|rect| {
-            let mut chunks = get_layout_chunks(rect.size());
-
-            let status_bar = draw_status_bar();
-
-            let tabs = draw_menu_tabs(&menu_titles, active_menu_item);
-
-            rect.render_widget(tabs, chunks[0]);
-
-            wrapper(rect, percentage_left, percentage_right, &mut node_list_state, &mut chunks, &git_explorer, repo);
-
-            rect.render_widget(status_bar, chunks[2]);
+            app::app(rect, &mut node_list_state, &git_explorer, repo, percentage_left, percentage_right);
         })?;
 
         if let Event::Key(key) = event::read()? {
