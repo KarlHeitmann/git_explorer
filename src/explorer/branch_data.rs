60711a9f54dba6b7a312ec004ef0db2351a5a957
Karl Heitmann <khf1988@gmail.com>
2023-03-21 18:38:03 -0300

    Added kind enum to BranchData

--- src/explorer/branch_data.rs
+++ src/explorer/branch_data.rs
@@ -2,9 +2,54 @@ use std::fmt::{Display, Formatter, Result as FmtResult};
 use git2::{Oid, Error, Branch, BranchType};
 
 #[derive(Clone)]
+enum BranchKind {
+    Local,
+    Remote,
+}
+
+impl From<BranchType> for BranchKind {
+    fn from(branch_type: BranchType) -> Self {
+        match branch_type {
+            BranchType::Local => BranchKind::Local,
+            BranchType::Remote => BranchKind::Remote,
+        }
+    }
+}
+
+
+#[derive(Clone)]
 pub struct BranchData {
     oid: Oid,
     shorthand: String,
+    kind: Option<BranchKind>,
+}
+
+// impl From<(Branch<'_>, BranchType)> for BranchData {
+impl From<Result<(Branch<'_>, BranchType), Error>> for BranchData {
+    // fn from(b: (Branch, BranchType)) -> Self {
+    fn from(b: Result<(Branch, BranchType), Error>) -> Self {
+        match b {
+            Ok((branch, branch_type)) => {
+                let reference = branch.get();
+                let shorthand = reference.shorthand().unwrap().to_string();
+                let oid = reference.target().unwrap();
+                let kind = BranchKind::from(branch_type);
+                Self {
+                    oid,
+                    shorthand,
+                    // kind: Some(kind.into()),
+                    kind: Some(kind),
+                }
+            },
+            Err(e) => {
+                Self {
+                    oid: Oid::zero(),
+                    shorthand: String::new(),
+                    kind: None,
+                }
+            }
+        }
+    }
 }
 
 impl BranchData {
@@ -15,15 +60,19 @@ impl BranchData {
                 let reference = branch.get();
                 let shorthand = reference.shorthand().unwrap().to_string();
                 let oid = reference.target().unwrap();
+                let kind = BranchKind::from(branch_type);
                 Self {
                     oid,
                     shorthand,
+                    // kind: Some(kind.into()),
+                    kind: Some(kind),
                 }
             },
             Err(e) => {
                 Self {
                     oid: Oid::zero(),
                     shorthand: String::new(),
+                    kind: None,
                 }
             }
         }
